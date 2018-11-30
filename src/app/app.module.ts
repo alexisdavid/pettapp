5352e48875f17ac481f95dfa828004a7c6e9b75e
alexisdavid <alexisduarte1512@gmail.com>
2018-11-29 23:54:02 -0500

    se inicia proyecto de pettapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgresComponent } from './pages/progres/progres.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgresComponent,
+    Grafica1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent
   ],
   imports: [
     BrowserModule
